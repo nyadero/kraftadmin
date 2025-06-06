bb423bbec1b3a8c52e577d247e8af4186977cb20
nyadero <briannyadero443@@gmail.com>
2025-06-06 23:27:28 +0300

    no need to track time to handle a request

--- kraft-core/src/main/java/com/bowerzlabs/config/WebMvcConfig.java
+++ kraft-core/src/main/java/com/bowerzlabs/config/WebMvcConfig.java
@@ -1,11 +1,8 @@
 package com.bowerzlabs.config;
 
-import com.bowerzlabs.components.RequestTimingInterceptor;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,22 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
-    private RequestTimingInterceptor requestTimingInterceptor;
-
-    @Autowired
-    private KraftrProperties properties;
-
-    @Autowired
-    private HttpSession session;
-
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(requestTimingInterceptor);
-//    }
+    private KraftProperties properties;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Adjust the classpath location to your actual library module's resource path
         registry.addResourceHandler("/js/**", "/css/**", "/uploads/**", "/images/**")
                 .addResourceLocations("classpath:/static/css/")
                 .addResourceLocations("/uploads/")
