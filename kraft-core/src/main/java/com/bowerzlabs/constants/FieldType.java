6346a82dcedbe3f81c2ea6a415fa2d5947cc0506
nyadero <briannyadero443@@gmail.com>
2025-06-08 23:52:47 +0300

    add more fieldtypes

--- kraft-core/src/main/java/com/bowerzlabs/constants/FieldType.java
+++ kraft-core/src/main/java/com/bowerzlabs/constants/FieldType.java
@@ -1,5 +1,5 @@
 package com.bowerzlabs.constants;
 
 public enum FieldType {
-    TEXT, IMAGE, DOCUMENT, VIDEO, AUDIO, LINK;
+    TEXT, IMAGE, DOCUMENT, VIDEO, AUDIO, LINK, COLOR, MAP, JSON, MARKDOWN, QRCODE;
 }
