1618f2de00f388bb22fe4e055ee1881452792aaa
nyadero <briannyadero443@@gmail.com>
2025-06-18 15:40:51 +0300

    renamed from KraftrJPAAutoConfiguration.java

---
+++ kraft-data-jpa/src/main/java/com/bowerzlabs/KraftJPAAutoConfiguration.java
@@ -0,0 +1,34 @@
+package com.bowerzlabs;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ImportAutoConfiguration({
+        DataSourceAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+//@EntityScan(basePackages = "com.bowerzlabs.models.kraftmodels")
+//@ComponentScan(basePackages = {"com.bowerzlabs.services", "com.bowerzlabs.repository", "com.bowerzlabs.utils"})
+public class KraftJPAAutoConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(KraftJPAAutoConfiguration.class);
+
+    @PostConstruct
+    public void init() {
+        log.info("Kraft JPA Auto Configuration Loaded!");
+    }
+
+    @Bean
+    public String jpa(){
+        System.out.println("Kraft jpa enabled");
+        return "Jpa Enabled";
+    }
+}
