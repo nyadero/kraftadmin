a3329ddffad56fe8fcb604d49f1b2fbcb03399ce
nyadero <briannyadero443@@gmail.com>
2025-06-18 15:43:35 +0300

    class to build analytic filters based on period and its current datetime

---
+++ kraft-core/src/main/java/com/bowerzlabs/dtos/PeriodFilter.java
@@ -0,0 +1,38 @@
+package com.bowerzlabs.dtos;
+
+import com.bowerzlabs.constants.TimePeriod;
+
+import java.time.LocalDateTime;
+
+public class PeriodFilter {
+    private final TimePeriod period;
+    private final LocalDateTime time;
+
+    public PeriodFilter(TimePeriod period) {
+        this.period = period;
+        time = switch (period) {
+            case MINUTE -> LocalDateTime.now().minusMinutes(1);
+            case HOUR -> LocalDateTime.now().minusHours(1);
+            case DAY -> LocalDateTime.now().minusDays(1);
+            case WEEK -> LocalDateTime.now().minusDays(7);
+            case MONTH -> LocalDateTime.now().minusMonths(1);
+            case YEAR -> LocalDateTime.now().minusYears(1);
+        };
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public TimePeriod getPeriod() {
+        return period;
+    }
+
+    @Override
+    public String toString() {
+        return "PeriodFilter{" +
+                "time=" + time +
+                ", period=" + period +
+                '}';
+    }
+}
