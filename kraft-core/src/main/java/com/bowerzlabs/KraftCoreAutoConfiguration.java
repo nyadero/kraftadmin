caa32087a278c5b922efba4c0348dceb2c471eca
nyadero <briannyadero443@@gmail.com>
2025-06-18 15:40:37 +0300

    renamed from KraftrCoreAutoConfiguration.java

---
+++ kraft-core/src/main/java/com/bowerzlabs/KraftCoreAutoConfiguration.java
@@ -0,0 +1,25 @@
+package com.bowerzlabs;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KraftCoreAutoConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(KraftCoreAutoConfiguration.class);
+
+    @PostConstruct
+    public void init() {
+        log.info(" Kraft core Initialized");
+    }
+
+    @Bean
+    public String helloKraftCore() {
+        System.out.println("Kraft core enabled");
+        return "KraftActive";
+    }
+
+}
