5af5dc68baf21d4d589c2f6f10ae1b79a551c63c
nyadero <briannyadero443@@gmail.com>
2025-06-21 22:26:42 +0300

    datetime field strategy

---
+++ kraft-ui/src/main/java/com/bowerzlabs/formfields/strategies/DateTimeFieldStrategy.java
@@ -0,0 +1,36 @@
+package com.bowerzlabs.formfields.strategies;
+
+import com.bowerzlabs.annotations.FormInputType;
+import com.bowerzlabs.database.DbObjectSchema;
+import com.bowerzlabs.formfields.DateTimeField;
+import com.bowerzlabs.formfields.FormField;
+import jakarta.persistence.metamodel.EntityType;
+
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.bowerzlabs.formfields.FormFieldFactory.extractRequiredValidation;
+import static com.bowerzlabs.formfields.FormFieldFactory.extractValue;
+
+public class DateTimeFieldStrategy implements FormFieldStrategy {
+    @Override
+    public boolean supports(Field field, DbObjectSchema dbObjectSchema) {
+        if (field.isAnnotationPresent(FormInputType.class)) {
+            FormInputType formInputType = field.getAnnotation(FormInputType.class);
+            return formInputType.value().equals(FormInputType.Type.DATETIME);
+        }
+        return field.getType() == LocalDateTime.class;
+    }
+
+
+    @Override
+    public FormField createField(Field field, DbObjectSchema dbObjectSchema, String inputName, boolean isSearch, List<EntityType<?>> subTypes) {
+        String label = FormField.formatLabel(field.getName());
+        String placeholder = "Enter " + label;
+        Object value = extractValue(field, dbObjectSchema);
+
+        boolean required = extractRequiredValidation(dbObjectSchema.getValidationRules(), field);
+        return new DateTimeField(label, placeholder, required, (String) value, inputName, dbObjectSchema.getValidationErrors(), dbObjectSchema.getValidationRules());
+    }
+}
