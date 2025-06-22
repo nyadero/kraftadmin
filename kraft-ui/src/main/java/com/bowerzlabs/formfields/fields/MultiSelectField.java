29fc8be27f72bf0780e3d7d1e3ea4eb00dad5998
nyadero <briannyadero443@@gmail.com>
2025-06-22 07:33:31 +0300

    multiselect input field

---
+++ kraft-ui/src/main/java/com/bowerzlabs/formfields/fields/MultiSelectField.java
@@ -0,0 +1,79 @@
+package com.bowerzlabs.formfields.fields;
+
+import com.bowerzlabs.formfields.FormField;
+
+import java.util.Map;
+
+public class MultiSelectField extends FormField {
+    /**
+     * returns the form-field label
+     */
+    @Override
+    public String getLabel() {
+        return "";
+    }
+
+    /**
+     * returns the form-field type based on the entity field type
+     */
+    @Override
+    public String getType() {
+        return "";
+    }
+
+    /**
+     * returns the form-field name based on the entity field name
+     */
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    /**
+     * returns the form-field placeholder
+     */
+    @Override
+    public String getPlaceholder() {
+        return "";
+    }
+
+    /**
+     * returns the form-field required state
+     */
+    @Override
+    public boolean getRequired() {
+        return false;
+    }
+
+    /**
+     * returns the form-field value from the field
+     */
+    @Override
+    public Object getValue() {
+        return null;
+    }
+
+    /**
+     * returns the form-field validation errors
+     */
+    @Override
+    public Map<String, String> getValidationErrors() {
+        return Map.of();
+    }
+
+    /**
+     * returns the form-field validation errors
+     */
+    @Override
+    public Map<String, String> getValidationRules() {
+        return Map.of();
+    }
+
+    /**
+     * builds the input model data
+     */
+    @Override
+    public Map<String, Object> getModelData() {
+        return Map.of();
+    }
+}
