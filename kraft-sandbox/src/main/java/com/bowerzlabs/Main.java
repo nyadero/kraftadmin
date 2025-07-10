7c36d225f70a2402ce779ac42fd9efda2f335af3
nyadero <briannyadero443@@gmail.com>
2025-07-10 07:41:52 +0300

    make sandbox async

--- kraft-sandbox/src/main/java/com/bowerzlabs/Main.java
+++ kraft-sandbox/src/main/java/com/bowerzlabs/Main.java
@@ -3,11 +3,11 @@ package com.bowerzlabs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.bowerzlabs"})
-//@EnableScheduling
-//@EnableCaching
+@EnableAsync
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
