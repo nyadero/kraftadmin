4f3a57113920f2a1feea600594bcc0fb3ad3c7de
nyadero <briannyadero443@@gmail.com>
2025-07-18 12:07:29 +0300

    delete @ComponentScan

--- kraft-sandbox/src/main/java/com/bowerzlabs/Main.java
+++ kraft-sandbox/src/main/java/com/bowerzlabs/Main.java
@@ -2,11 +2,9 @@ package com.bowerzlabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.bowerzlabs"})
 @EnableAsync
 public class Main {
     public static void main(String[] args) {
