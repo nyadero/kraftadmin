0dbe56513df86e71354430ad3d465dbdcfa4d81a
nyadero <briannyadero443@@gmail.com>
2025-07-18 12:02:00 +0300

    comment out the logs

--- kraft-data-jpa/src/main/java/com/bowerzlabs/EntityValueAccessor.java
+++ kraft-data-jpa/src/main/java/com/bowerzlabs/EntityValueAccessor.java
@@ -18,71 +18,89 @@ public class EntityValueAccessor {
     private static final Logger log = LoggerFactory.getLogger(EntityValueAccessor.class);
 
     public static void setFieldValue(EntityType<?> clazz, Object target, String path, String value, EntityManager em) throws Exception {
-        try{
-        String[] parts = path.split("\\.");
-        Object current = target;
-        Class<?> currentClass = target.getClass();
+        try {
+            String[] parts = path.split("\\.");
+            Object current = target;
 
-        List<Field> allFields = getAllFields(clazz.getJavaType());
-        Map<String, Field> fieldMap = new HashMap<>();
-        for (Field f : allFields) {
-            f.setAccessible(true);
-            fieldMap.put(f.getName(), f);
-        }
+            // Start with fields from the main entity
+            List<Field> allFields = getAllFields(clazz.getJavaType());
+            Map<String, Field> fieldMap = new HashMap<>();
+            for (Field f : allFields) {
+                f.setAccessible(true);
+                fieldMap.put(f.getName(), f);
+            }
 
-        for (int i = 0; i < parts.length; i++) {
-            String part = parts[i];
-            Field field = fieldMap.get(part);
-            if (field == null) continue;
+            for (int i = 0; i < parts.length; i++) {
+                String part = parts[i];
+                Field field = fieldMap.get(part);
+                if (field == null) {
+//                    log.warn("Field '{}' not found in class {}", part, current.getClass().getSimpleName());
+                    continue;
+                }
 
-            if (i == parts.length - 1) {
-                Class<?> fieldType = field.getType();
-                log.info("fieldType {} ", field.getName());
+                if (i == parts.length - 1) {
+                    // This is the final field - set its value
+                    Class<?> fieldType = field.getType();
+//                    log.info("Setting field {} of type {} with value {} on object {}",
+//                            field.getName(), fieldType.getSimpleName(), value, current.getClass().getSimpleName());
 
-                if (field.isAnnotationPresent(ManyToOne.class) || field.isAnnotationPresent(OneToOne.class)) {
-                    Class<?> relatedClass = fieldType;
-                    Field idField = getIdField(relatedClass);
-                    if (idField == null)
-                        throw new IllegalArgumentException("No @Id field in " + relatedClass.getSimpleName());
+                    if (field.isAnnotationPresent(ManyToOne.class) || field.isAnnotationPresent(OneToOne.class)) {
+                        // Handle relationship fields
+                        Class<?> relatedClass = fieldType;
+                        Field idField = getIdField(relatedClass);
+                        if (idField == null) {
+//                            throw new IllegalArgumentException("No @Id field in " + relatedClass.getSimpleName());
+                            continue;
+                        }
 
-                    Object id = convertValue(idField.getType(), value);
-                    Object relatedEntity = em.find(relatedClass, id);
+                        Object id = convertValue(idField.getType(), value);
+                        Object relatedEntity = em.find(relatedClass, id);
 
-                    if (relatedEntity == null) {
-                        relatedEntity = relatedClass.getDeclaredConstructor().newInstance();
-                        idField.setAccessible(true);
-                        idField.set(relatedEntity, id);
-                        em.persist(relatedEntity);
-                        log.info("Auto-created {} with ID {}", relatedClass.getSimpleName(), id);
+                        if (relatedEntity == null) {
+                            relatedEntity = relatedClass.getDeclaredConstructor().newInstance();
+                            idField.setAccessible(true);
+                            idField.set(relatedEntity, id);
+                            em.persist(relatedEntity);
+//                            log.info("Auto-created {} with ID {}", relatedClass.getSimpleName(), id);
+                        }
+
+                        field.set(current, relatedEntity);
+                    } else {
+                        // Handle regular fields
+                        Object convertedValue;
+                        if (field.getName().contains("password")) {
+                            convertedValue = new BCryptPasswordEncoder(6).encode(value);
+//                            log.info("Password field {} encoded", field.getName());
+                        } else {
+                            convertedValue = convertValue(fieldType, value);
+                        }
+                        field.set(current, convertedValue);
                     }
-
-                    field.set(target, relatedEntity);
                 } else {
-                    if (field.getName().contains("password")) {
-                        field.set(target, new BCryptPasswordEncoder(6).encode(value));
-                        log.info("fieldType is password {} {} {} {}", field.getName().contains("password"), value, new BCryptPasswordEncoder(6).encode(value), target);
+                    // This is a nested field - navigate to it
+                    Object nested = field.get(current);
+                    if (nested == null) {
+                        // Create the nested object
+                        nested = field.getType().getDeclaredConstructor().newInstance();
+                        field.set(current, nested);
+//                        log.info("Created nested object of type {} for field {}", field.getType().getSimpleName(), field.getName());
                     }
-                    Object convertedValue = convertValue(fieldType, value);
-                    field.set(target, convertedValue);
-                }
-            } else {
-                Object nested = field.get(current);
-                if (nested == null) {
-                    nested = field.getType().getDeclaredConstructor().newInstance();
-                    field.set(current, nested);
-                }
-                current = nested;
-                currentClass = nested.getClass();
-                allFields = getAllFields(currentClass);
-                fieldMap.clear();
-                for (Field f : allFields) {
-                    f.setAccessible(true);
-                    fieldMap.put(f.getName(), f);
+
+                    // Move to the nested object
+                    current = nested;
+
+                    // Update field map for the nested object's class
+                    allFields = getAllFields(nested.getClass());
+                    fieldMap.clear();
+                    for (Field f : allFields) {
+                        f.setAccessible(true);
+                        fieldMap.put(f.getName(), f);
+                    }
                 }
             }
-        }} catch (Exception e) {
-            log.info("exception {}", (Object) e.getStackTrace());
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            log.error("Exception setting field value for path: {}, value: {}, target: {}", path, value, target.getClass().getSimpleName(), e);
+//            throw new RuntimeException(e);
         }
     }
 
@@ -107,5 +125,4 @@ public class EntityValueAccessor {
         }
         return fields;
     }
-
 }
